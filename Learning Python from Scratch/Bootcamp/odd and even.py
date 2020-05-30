04894b30007d54edad58999e09180dd8efdab89f
Jeny Susan Rajan <37576359+jenysusan@users.noreply.github.com>
2020-05-31 03:23:34 +0530

    Add files via upload

---
+++ Learning Python from Scratch/Bootcamp/odd and even.py
@@ -0,0 +1,7 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input())
+if a%2==0:
+    print("even")
+else:
+    print("odd")
